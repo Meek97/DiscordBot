98a242c1733a70504b0d71a521255c57333c4a53
Brian <b.meek97@gmail.com>
2022-01-04 02:37:36 -0500

    fixed an issue where the month was not displayed properly in the logger

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 module.exports = {
 	async log(args) {
 		const currentDate = new Date();
-		const timeStamp = `[${currentDate.getMonth()}.${currentDate.getDate()}.${currentDate.getFullYear()}|${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}:${currentDate.getMilliseconds()}]  `;
+		const timeStamp = `[${currentDate.getMonth() + 1}.${currentDate.getDate()}.${currentDate.getFullYear()}|${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}:${currentDate.getMilliseconds()}]  `;
 		let logMessage = '';
 		for (let i = 0; i < args.length;i++) {
 			logMessage += args[i];
