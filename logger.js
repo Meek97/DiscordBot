dd976163a14bbc993d0424808b00a0aa65bb0693
Brian Meek <b.meek97@gmail.com>
2022-08-17 14:50:39 -0400

    Update logger formatting

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 module.exports = {
 	async log(args) {
 		const currentDate = new Date();
-		const timeStamp = `[${currentDate.getMonth() + 1}.${currentDate.getDate()}.${currentDate.getFullYear()}|${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}:${currentDate.getMilliseconds()}]  `;
+		const timeStamp = `[${(currentDate.getMonth() + 1).toString().padStart(2,'0')}.${currentDate.getDate().toString().padStart(2,'0')}.${currentDate.getFullYear()}|${currentDate.getHours().toString().padStart(2,'0')}:${currentDate.getMinutes().toString().padStart(2,'0')}:${currentDate.getSeconds().toString().padStart(2,'0')}:${currentDate.getMilliseconds().toString().padStart(3,'0')}]  `;
 		let logMessage = '';
 		for (let i = 0; i < args.length;i++) {
 			logMessage += args[i];
