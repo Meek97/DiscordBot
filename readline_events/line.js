c49ef9c255ee60c24c6c30fd954c7ced0de8e183
Brian Meek <b.meek97@gmail.com>
2022-08-24 14:00:40 -0400

    cleaned up line.js event

--- readline_events/line.js
+++ readline_events/line.js
@@ -1,9 +1,5 @@
-const mongoDriver = require('../MongoDriver');
 const logger = require('../logger');
-const weather = require('../weather');
 const bot = require('../memebot');
-const { SUBMISSIONS_DB, CHANNELS_DB } = require('../config.json');
-const { MessagePayload, MessageEmbed, AttachmentBuilder, MessageAttachment } = require('discord.js');
 
 module.exports = {
 	name: 'line',
