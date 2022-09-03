a15b8d0b0337d53132963436705af7b0a33e6fe2
Brian Meek <b.meek97@gmail.com>
2022-09-03 12:56:27 -0400

    added a CLI command to send a simple text message to a channel

--- readline_events/line.js
+++ readline_events/line.js
@@ -1,15 +1,33 @@
+const { MessagePayload } = require('discord.js');
 const logger = require('../logger');
 const bot = require('../memebot');
+const mongooseDriver = require('../mongooseDriver');
 
 module.exports = {
 	name: 'line',
 	once: false,
 	execute(input) {
-		logger.log(`Received: ${input}`);
-		if(input == 'weather'){
-				
+		const inputParts = input.split(' ');
+		if(inputParts.length > 3) {
+			while(inputParts.length > 3) {
+				inputParts[2] += ` ${inputParts[3]}`;
+				const removed = inputParts.splice(3,1)
+			}
 		}
-		if(input == 'morning'){
+		logger.log(`Console Input Received: [${input}]`);
+		if(inputParts[0] == '/say'){
+				mongooseDriver.Channels.find({'name':inputParts[1]})
+					.then(function(channelResults) {
+						if(channelResults.length > 0){
+							bot.GetChannel(channelResults[0]._id)
+								.then((channel) => {
+									bot.SendMessage(new MessagePayload(channel,
+										{content:inputParts[2]}));
+								});
+						}
+					});
+		}
+		if(inputParts[0] == '/morning'){
 			bot.SendGoodMorning();
 		}
 	},
